ce5118cec83686e209cd9d80d7fd453654c2ab94
Valik Kinakh <valik.kinah@gmail.com>
2021-06-21 16:56:34 +0300

    Added new feature: highlited elements

--- Task_Calendar/main.js
+++ Task_Calendar/main.js
@@ -16,11 +16,9 @@ const renderCalendar = () => {
   document.querySelector(".date h1").innerHTML = months[date.getMonth()]+" "+date.getFullYear();
 
   let days = "";
-
   for (let x = firstDayIndex; x > 0; x--) {
     days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
   }
-
   for (let i = 1; i <= lastDay; i++) {
     if (i === new Date().getDate() && date.getMonth() === new Date().getMonth() && date.getFullYear()===new Date().getFullYear()) {
       days += `<div class="today">${i}</div>`;
@@ -28,25 +26,46 @@ const renderCalendar = () => {
       days += `<div>${i}</div>`;
     }
   }
-
   for (let j = 1; j <= nextDays; j++) {
     days += `<div class="next-date">${j}</div>`;
   }
   monthDays.innerHTML = days;
 };
 
-document.getElementById("prev").addEventListener("click", () => {
-  date.setMonth(date.getMonth() - 1);
-  renderCalendar();
-  redDays();
-});
-
 document.getElementById("next").addEventListener("click", () => {
   date.setMonth(date.getMonth() + 1);
   renderCalendar();
   redDays();
+  chosenElem();
 });
 
+document.getElementById("prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+  redDays();
+  chosenElem();
+});
+
+function chosenElem() {
+  let selectedElem;
+
+document.getElementsByClassName("days")[0].onclick = function(event) {
+  let target = event.target;
+  highlight(target);
+};
+
+function highlight(elem) {
+  if (selectedElem) { 
+    selectedElem.style.backgroundColor="white";
+  selectedElem.style.color="black";
+  }
+  selectedElem = elem;
+  selectedElem.style.backgroundColor="blue";
+  selectedElem.style.color="white";
+  
+}
+}
+chosenElem();
 function redDays() {
   const allDivDays=document.getElementsByClassName("days")[0];
   allDivDays.children[0].style.color="red";
